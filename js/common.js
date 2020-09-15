31c5241873551fab22625f8692a23d50b918e4a8
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-09-15 14:28:57 +0300

    1

--- js/common.js
+++ js/common.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
   //Всплывающее меню
   jQuery('.header-right__cats ul li.cat-parent').hover(function(){
+    jQuery(this).addClass('menu-active');
+    console.log(jQuery(this));
     jQuery(this).children('ul').css('display', 'flex');
   }, function(){
     jQuery(this).children('ul').css('display', 'none');
+    jQuery(this).removeClass('menu-active');
   });
 });
