1bf60a3927892253d1e6c609ab9304bed0756e9f
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-09-19 20:13:50 +0300

    1

--- js/common.js
+++ js/common.js
@@ -1,13 +1,31 @@
 $(document).ready(function () {
   //Всплывающее меню
-  jQuery('.header-right__cats ul li.cat-parent').hover(function(){
-    jQuery(this).addClass('menu-active');
-    console.log(jQuery(this));
-    jQuery(this).children('ul').css('display', 'flex');
-  }, function(){
-    jQuery(this).children('ul').css('display', 'none');
-    jQuery(this).removeClass('menu-active');
-  });
+  if (jQuery(window).width() < 992) {
+    jQuery('.header-right__cats ul li.cat-parent').hover(function(){
+      jQuery(this).children('ul').slideDown(200);
+      jQuery(this).addClass('cat-parent-active');
+    }, function(){
+      jQuery(this).children('ul').slideUp(200);
+      jQuery(this).removeClass('cat-parent-active');
+    });
+
+    jQuery('.open-menu__row .close').on('click', function() {
+      jQuery('.open-menu').slideUp(300);
+    });
+    jQuery('.header-mob__row .burger').on('click', function() {
+      jQuery('.open-menu').slideDown(300);
+    });
+  } else {
+    jQuery('.header-right__cats ul li.cat-parent').hover(function(){
+      jQuery(this).addClass('menu-active');
+      console.log(jQuery(this));
+      jQuery(this).children('ul').css('display', 'flex');
+    }, function(){
+      jQuery(this).children('ul').css('display', 'none');
+      jQuery(this).removeClass('menu-active');
+    });
+  }
+  
   //feed slider
   $('.feed-wrap__slider').slick({
     centerMode: false,
